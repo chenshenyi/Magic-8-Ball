85180002ee5efe29b60b2543e1f99a668f0d168c
CHEN-SHEN-YI <chen-shen-yi@macbook.fdorm5.nccu.edu.tw>
2017-12-03 22:56:17 +0800

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by CHEN-SHEN-YI on 2017/12/3.
+//  Copyright © 2017年 Aptan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
